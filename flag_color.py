5979f935a10d0e5e58608fb24ac077b87c4acc07
Luuk Looijenga <luuk@MacBook-Air-van-Luuk.local>
2015-02-23 10:25:54 +0100

    Updated versie met werkend random color vak

--- flag_color.py
+++ flag_color.py
@@ -1,35 +1,60 @@
+#!/usr/local/bin/python3
+
+from PyQt4 import QtCore, QtGui
 import sys
-from PyQt4 import QtGui
 from random import randrange
 
 class FlagColor(QtGui.QWidget):
-    
     def __init__(self):
-        super(FlagColor, self).__init__()
+        QtGui.QWidget.__init__(self)
+        self.setGeometry(300, 300, 350, 350)
+        self.setWindowTitle('Flag Color')
         self.initUI()
-    
+    def paintEvent(self, event):
+        paint = QtGui.QPainter()
+        paint.begin(self)
+
+        # White background
+        paint.setBrush(QtCore.Qt.white)
+        paint.drawRect(event.rect())
+        
+        paint.end()
+        
     def initUI(self):
-        self.setGeometry(300,300,250,150)
-        self.setWindowTitle("Super mooi")
+
         
         self.col = QtGui.QColor(0, 0, 0)  
         
-        self.col.setRed(100) 
-        self.col.setBlue(50)
-        self.col.setGreen(150)
+        random = QtGui.QPushButton('Random', self)
+        random.setCheckable(True)
+        random.move(10, 10)
+
+        random.clicked[bool].connect(self.setColor)
         
         self.square = QtGui.QFrame(self)
-        self.square.setGeometry(150, 20, 100, 100)
-        self.square.setStyleSheet("QWidget { background-color: %s }" %  
-        self.col.name())
+        self.square.setGeometry(150, 100, 100, 100)
+        self.square.setStyleSheet("QWidget { background-color: %s }" % self.col.name())
         
-        self.show
-    
-def main():
+        self.show()
+        
+    def setColor(self, pressed):
+        source = self.sender()
+        
+        
+        val, val1, val2 = randrange(0,255), randrange(0,255), randrange(0,255)
+        
+                        
+        if source.text() == "Random":
+            self.col.setRed(val)
+            self.col.setBlue(val1)
+            self.col.setGreen(val2)
+            
+        self.square.setStyleSheet("QFrame { background-color: %s }" %
+                    self.col.name())
+        
+if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
+    
     flag = FlagColor()
+    
     app.exec_()
-    
-if __name__ == "__main__":
-    main()
-    
